7abdbcf03d2a96419aaec75b9b1751fb5ceaefd4
Abhishek Bharadwaj <abhishek.bharadwaj@healthify.co>
2019-12-15 00:15:32 +0530

    Added single select fragment

--- app/src/main/java/com/choiceview/MainActivity.kt
+++ app/src/main/java/com/choiceview/MainActivity.kt
@@ -1,54 +1,23 @@
 package com.choiceview
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.choiceadapter.Choice
-import com.choiceadapter.ChoiceAdapter
-import com.choiceadapter.ChoiceCallback
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.layout_test_choice.view.*
+import androidx.fragment.app.Fragment
+import com.choiceview.fragments.SingleSelectionFragment
 
-class MainActivity : AppCompatActivity(), ChoiceCallback {
-
-    private lateinit var choiceAdapter: ChoiceAdapter
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val options = mutableListOf<Option>()
-        for (i in 0..10) {
-            options.add(Option(i))
-        }
-
-        choiceAdapter = ChoiceAdapter(this, R.layout.layout_test_choice, this, 2, flexible = false)
-        choiceAdapter.updateData(options)
-        rv.layoutManager = LinearLayoutManager(this)
-        rv.adapter = choiceAdapter
+        addFragment(SingleSelectionFragment())
     }
 
-    override fun onChoiceSelected(choice: Choice, view: View) {
-        updateItemUI(choice, view, android.R.color.holo_blue_light, true)
-    }
-
-    override fun onChoiceUnselected(choice: Choice, view: View) {
-        updateItemUI(choice, view, android.R.color.holo_green_light, false)
-    }
-
-    override fun alreadySelectedMaxChoices() {
-        Toast.makeText(this, getString(R.string.max_choices_selected), Toast.LENGTH_SHORT).show()
-    }
-
-    private fun updateItemUI(choice: Choice, view: View, bgColor: Int, shouldCheck: Boolean) {
-        view.tv.text = choice.toString()
-        view.container.setCardBackgroundColor(ContextCompat.getColor(this@MainActivity, bgColor))
-        view.rb.apply {
-            isChecked = shouldCheck
-            setTag(R.id.choice_data, choice)
-        }
+    private fun addFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fl_container, fragment)
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/choiceview/fragments/SingleSelectionFragment.kt
@@ -0,0 +1,69 @@
+package com.choiceview.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.choiceadapter.Choice
+import com.choiceadapter.ChoiceAdapter
+import com.choiceadapter.ChoiceCallback
+import com.choiceview.R
+import com.choiceview.data.Option
+import kotlinx.android.synthetic.main.fragment_single_selection.*
+import kotlinx.android.synthetic.main.layout_test_choice.view.*
+
+class SingleSelectionFragment : Fragment(), ChoiceCallback {
+
+    private lateinit var choiceAdapter: ChoiceAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_single_selection, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val options = mutableListOf<Option>()
+        for (i in 0..10) {
+            options.add(Option(i))
+        }
+
+        choiceAdapter = ChoiceAdapter(
+            requireContext(), R.layout.layout_test_choice, this, 1
+        )
+        choiceAdapter.updateData(options)
+        rv_single_select.layoutManager = LinearLayoutManager(requireContext())
+        rv_single_select.adapter = choiceAdapter
+    }
+
+    override fun onChoiceSelected(choice: Choice, view: View) {
+        updateItemUI(choice, view, android.R.color.holo_blue_light, true)
+    }
+
+    override fun onChoiceUnselected(choice: Choice, view: View) {
+        updateItemUI(choice, view, android.R.color.holo_green_light, false)
+    }
+
+    override fun alreadySelectedMaxChoices() {
+        Toast.makeText(
+            requireContext(),
+            getString(R.string.max_choices_selected),
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun updateItemUI(choice: Choice, view: View, bgColor: Int, shouldCheck: Boolean) {
+        view.tv.text = choice.toString()
+        view.container.setCardBackgroundColor(ContextCompat.getColor(requireContext(), bgColor))
+        view.rb.apply {
+            isChecked = shouldCheck
+            setTag(R.id.choice_data, choice)
+        }
+    }
+}
